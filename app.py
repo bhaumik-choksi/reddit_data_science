22e384d8b963f144f69790e068d6b77317f23a70
Bhaumik <bhaumik@bhaumik.com>
2019-02-22 18:49:27 -0700

    Placeholder text added for app

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    return "We're the reddit group. Data science is cool!"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
